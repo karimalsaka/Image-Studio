6c354ae2c970b25f1184bd165a36a15d7bb53532
karimalsaka <karimalsaka@hotmail.com>
2026-04-06 22:33:36 +0100

    Fix CORS: reject without throwing error

--- server/index.ts
+++ server/index.ts
@@ -31,7 +31,7 @@ app.use(cors({
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'));
+            callback(null, false);
         }
     },
     credentials: true,
